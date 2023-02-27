6e083b4d73fe2e026facf060330f643f7ef2a2ed
Jacob Rubino <jacobrubino@gmail.com>
2023-02-27 12:55:50 -0500

    added interval etc

--- assets/codeq.js
+++ assets/codeq.js
@@ -3,9 +3,15 @@ var buttData = document.getElementById("botButt");
 var qheader = document.getElementById("question");
 var ansButts = ansList.querySelectorAll("button");
 var timerDis = document.getElementById("timeLeft");
+// var butto1= document.getElementById("#butt1")
+// var butto2= document.getElementById("#butt2")
+// var butto3= document.getElementById("#butt3")
+// var butto4= document.getElementById("#butt4")
 var timeScore = 80;
 var questionIndex = 0;
 var intervalID 
+var val
+
 //first call on the button to clear the innerhtml of the question and button, change it to submit, generate a list of answers and change h1 to the question
 const qArr = [
   "A very useful tool used during development and debugging for printing content to the debugger is:",
@@ -21,6 +27,7 @@ const correctArr = [
   "All of the above",
   "Alerts",
 ];
+
 const AnsArr = [
   ["JavaScript", "Terminal", "For Loops", "console.log"],
   ["Commas", "Curly brackets", "Qoutes", "Parentheses"],
@@ -32,7 +39,24 @@ const AnsArr = [
 function clearInner() {
   qheader.textContent = "";
 }
+//check if answer is right or wrong, check for button click, and check if the content of that button matches the answer at the index that matches the questionIndex
+// function checkRight(){
+//   if (correctArr[questionIndex] == source.innertext){
+//     timeScore = timeScore - 10
+//     }
+// }
+function checkRight(clicked_id) {
+  a = document.getElementById(clicked_id).innerText
+  b = correctArr[questionIndex]
+  console.log(a)
+  // console.log(typeof(correctArr[questionIndex]))
+  if (a == b){ 
+    timeScore ++
+  } else {
+    timeScore = timeScore - 10
+  }
 
+}
 function printQuestion() {
   qheader.textContent = qArr[questionIndex];
 }
@@ -42,6 +66,7 @@ function printLi() {
     const answer = AnsArr[questionIndex][index];
     ansButts[index].textContent = answer;
   }
+
 }
 
 //i want this to get rid of the button, reveal the list (needs to be hidden first), and loop through an array to list possible answers on the LI, create a new button that will be submit or make the options clickable.
@@ -60,8 +85,14 @@ botButt.addEventListener("click", function () {
     timerDis.textContent = "You have 1 Second left"
   } else {
     timerDis.textContent = "You have run out of time"
-  }
+    ansList.hidden = true;
+    qheader.textContent = "Sorry, better luck next time!"
+  } 
+  
 }, 1000);
+  if (questionIndex > 4) {
+    ansList.hidden = true 
+}
 });
 
 for (let index = 0; index < ansButts.length; index++) {
@@ -70,7 +101,7 @@ for (let index = 0; index < ansButts.length; index++) {
     var currentAns = answerbutton.textContent;
     console.log(currentAns);
     var correctAns = correctArr[questionIndex]
-    console.log(correctAns)
+    console.log(correctAns);
     questionIndex++
     printQuestion()
     printLi()
